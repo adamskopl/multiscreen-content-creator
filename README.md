13032036c0629040631ff9824a6d36d86eef8670
Adam Skobodziński <adam@skobo.pl>
2021-12-13 22:06:07 +0100

    start update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # multiscreen-content-creator
+
+# purpose of project
+
+- to display some content in multiple browser windows on multiple devices
+- to manage the way connected devices display content through an editor
+
+# install
+
+- `npm install`
+- `npm run start` or `./start.sh`
--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# run after npm install
-
-mkdir libs
+mkdir -p libs
 cp node_modules/socket.io-client/dist/socket.io.js* node_modules/vue/dist/vue.js node_modules/normalize.css/normalize.css libs
+nodemon --experimental-modules --ignore client-device --ignore client-editor server/server.mjs
